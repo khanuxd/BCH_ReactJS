a5da1d09ff3ebea48d6aa9f6585aadaaa4cbf2f5
Khan Fayjul <khanuxd@gmail.com>
2021-03-29 12:26:37 +0300

    converting into state property

---
+++ project-boxes/src/Box.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box = (props) => {
+        return (
+            <div className="box">
+                <h1>{props.name}</h1>
+                <p>Age: {props.age}</p>
+                <p>Title: {props.title}</p>
+            </div>
+        );
+    };
+
+    export default Box;
--- project-boxes/src/Main.js
+++ project-boxes/src/Main.js
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
+import Box from "./Box";
 
-const Box = (props) => {
-    return (
-        <div className="box">
-            <h1>{props.name}</h1>
-            <p>Age: {props.age}</p>
-            <p>Title: {props.title}</p>
-        </div>
-    );
-};
+class Main extends Component {
+    state = {
+        persons: [
+            {
+                name: "Khan",
+                age: 25,
+                title: "CEO",
+            },
+            {
+                name: "Smith",
+                age: 31,
+                title: "Designer",
+            },
+            {
+                name: "Ronn",
+                age: 41,
+                title: "Developer",
+            }
+        ]
+    }
 
-const Main = () => {
-    return (
-        <main>
-            <div>
-                <Box name="Khan" age="31" title="CEO" />
-                <Box name="Matt" age="35" title="FullStack Developer" />
-                <Box name="Smith" age="25" title="Manager" />
-            </div>
-        </main>
-    );
-};
+    render() {
+        return (
+            <main>
+                <div>
+                    <Box
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                        title={this.state.persons[0].title}
+                    />
+                    <Box
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        title={this.state.persons[1].title}
+                    />
+                    <Box
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}
+                        title={this.state.persons[2].title}
+                    />
+                </div>
+            </main>
+        )
+    }
+}
+
+/* class Main extends Component {
+    render() {
+        return (
+            <main>
+                <div>
+                    <Box name="Khan" age="31" title="CEO" />
+                    <Box name="Matt" age="35" title="FullStack Developer" />
+                    <Box name="Smith" age="25" title="Manager" />
+                </div>
+            </main>
+        )
+    }
+} */
 
 export default Main;
