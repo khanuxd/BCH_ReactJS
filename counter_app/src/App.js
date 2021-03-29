8030f51a5ffdd3cb30caa1c624ea1c550e20f45f
Khan Fayjul <khanuxd@gmail.com>
2021-03-29 14:16:32 +0300

    add button functionality console

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 20,
+  };
+
+  addHandler = () => {
+    console.log('clicked addHand');
+  };
+
+  removeHandler = () => {
+    console.log('clicked removeHand');
+  };
+
+  resetHandler = () => {
+    console.log('clicked resetHand');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="circle">{this.state.counter}</h1>
+        <button onClick={this.addHandler}>Add 1</button>
+        <button onClick={this.removeHandler}>Remove 1</button>
+        <button onClick={this.resetHandler}>Reset</button>
+      </div>
+    );
+  }
+
 };
 
 export default App;
