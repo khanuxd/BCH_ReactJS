51e67c3f0546d36fa37de25440118fda67b118e8
Khan Fayjul <khanuxd@gmail.com>
2021-04-22 14:37:45 +0300

    using map method in the box

--- project-boxes/src/Components/Main.js
+++ project-boxes/src/Components/Main.js
@@ -5,16 +5,25 @@ class Main extends Component {
     state = {
         persons: [
             {
+                id: 1,
                 name: "Khan",
                 age: 25,
                 title: "CEO",
             },
             {
+                id: 2,
                 name: "Smith",
                 age: 31,
                 title: "Designer",
             },
             {
+                id: 3,
+                name: "Ronn",
+                age: 41,
+                title: "Developer",
+            },
+            {
+                id: 4,
                 name: "Ronn",
                 age: 41,
                 title: "Developer",
@@ -49,7 +58,10 @@ class Main extends Component {
             <main>
                 <button onClick={this.handleClick}>Click Me on the main page</button>
                 <div>
-                    <Box
+                    {this.state.persons.map(i => {
+                        return <Box name={i.name} age={i.age} title={i.title} key={i.id} /> })
+                        }
+                    {/* <Box
                         name={this.state.persons[0].name}
                         age={this.state.persons[0].age}
                         title={this.state.persons[0].title}
@@ -63,7 +75,7 @@ class Main extends Component {
                         name={this.state.persons[2].name}
                         age={this.state.persons[2].age}
                         title={this.state.persons[2].title}
-                    />
+                    /> */}
                 </div>
             </main>
         )
