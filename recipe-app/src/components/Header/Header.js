bde5dee4eaf7917994052188a13046d0f36929f8
Khan Fayjul <khanuxd@gmail.com>
2021-05-16 11:46:33 +0300

    Header.js classComponent to functiona lComponent conversion

--- recipe-app/src/components/Header/Header.js
+++ recipe-app/src/components/Header/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Nav from '../Main/Nav';
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="nav-top">
-                <div className="logo">
-                    <a href="/">Logo</a>
-                </div>
-                <Nav />
-            </header>
-        );
-    }
-}
+const Header = () => {
+    return (
+        <header className="nav-top">
+            <div className="logo">
+                <a href="/">Recipe</a>
+            </div>
+            <Nav />
+        </header>
+    );
+};
 
 export default Header;
