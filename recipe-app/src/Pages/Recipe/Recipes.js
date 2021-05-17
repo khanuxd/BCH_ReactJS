a8f6d522fa7c7a98c87d09ab4f7a3f88ca7616d0
Khan Fayjul <khanuxd@gmail.com>
2021-05-17 08:16:56 +0300

    loading Spiner added on the Recipies.js

--- recipe-app/src/Pages/Recipe/Recipes.js
+++ recipe-app/src/Pages/Recipe/Recipes.js
@@ -2,31 +2,37 @@ import React, { useState, useEffect } from 'react';
 import RecipesList from '../../components/Main/Recipes/RecipesList';
 
 import axios from "axios";
-
+import { Spinner } from 'react-bootstrap';
 
 const Recipes = () => {
 
     const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
 
         const allRecipes = async () => {
 
             try {
-                const recipeRes = await axios.get("http://localhost:3001/recipes")
+                const recipeRes = await axios
+                    .get("http://localhost:3001/recipes");
                 setRecipes(recipeRes.data);
+                setLoading(true);
             } catch (error) {
                 console.log('error getting data:' + error);
             }
         };
         allRecipes();
-
     }, []);
 
     return (
-        <div>
-            <RecipesList recipes={recipes} />
-        </div>
+        <>
+            {loading ? <RecipesList recipes={recipes} /> : (
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            )}
+        </>
     );
 };
 
