2f4ec31970aaff74329e1d411de1f65b7c0bd733
Khan Fayjul <khanuxd@gmail.com>
2021-05-18 21:27:18 +0300

    AAbou

--- recipe-app/src/Pages/About/About.js
+++ recipe-app/src/Pages/About/About.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './About.css';
 
-class About extends Component {
-    render() {
-        return (
-            <div className="about-us">
-                <h1>This is our About Page</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                <h1>This is our About Page</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                <h1>This is our About Page</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>
-        );
-    }
-}
+const About = () => {
+    return (
+        <div className="about-us">
+            <h1>About Page</h1>
+            <p>
+                This is a Recipe App  for the GET and POST request. It was developed with ReactJS for the front-end and Php Symfony for the back-end.
+            </p>
+        </div>
+    );
+};
 
 export default About;
