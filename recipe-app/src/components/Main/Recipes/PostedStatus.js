1969e36830ccf5ac209882d7940532d397d49442
Khan Fayjul <khanuxd@gmail.com>
2021-05-18 21:11:36 +0300

    AddNewRecipe overlay style

--- recipe-app/src/components/Main/Recipes/PostedStatus.js
+++ recipe-app/src/components/Main/Recipes/PostedStatus.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../../../Pages/Recipe/AddNewRecipe.css';
 
 const closeHandler = () => {
     window.location.reload();
 }
 
-const PostedStatus = ({name}) => {
+const PostedStatus = ({ name }) => {
     return (
         <div className="overlay">
             <div className="postedUpdate">
